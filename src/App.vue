5dd0ab4534e755230808e2f2dd9f0243d78e2e88
Vecheslav-Eremeev <eremeev-vecheslav@yandex.ru>
2023-12-12 15:33:31 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="app">
+    <div class="header container h-100 p-5">
+      <h1 class="mb-5">{{ title }}</h1>
+      <div class="d-flex justify-content-center h-100">
+        <div class="searchbar w-50 mx-2">
+          <input
+            type="text"
+            class="input form-control"
+            v-model="city"
+            placeholder="Введите название города.."
+          />
+        </div>
+        <button class="btn btn-search btn-primary" @click="searchWeather">
+          Поиск <i class="fas fa-search"></i>
+        </button>
+      </div>
+    </div>
+    <br>
+    <TheWeather :city="city" v-if="showWeather"/>
+  </div>
+</template>
+
+<script>
+import TheWeather from './components/TheWeather.vue';
+
+export default {
+  name: "App",
+  components: {
+    TheWeather,
+  },
+  data() {
+    return {
+      city: "",
+      showWeather: false,
+      title: 'Погодное приложение'
+    };
+  },
+  methods: {
+    async searchWeather() {
+      this.showWeather = false;
+      await this.$nextTick();
+      this.showWeather = true;
+    },
+  },
+};
+</script>
+
+<style>
+@import url('./assets/css/main.css');
+</style>
---
+++ src/components/TheWeather.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="container p-0">
+    <div class="d-flex" v-if="success">
+      <div class="card main-div w-100">
+        <div class="p-3">
+          <h2 class="mb-1 day">Сегодня</h2>
+          <p class="text-light date mb-0">{{ localDate }}</p>
+          <small>{{ localTime }}</small>
+          <h2 class="place">
+            <i class="fa fa-location"
+              >{{ name }} <small>{{ country }}</small></i
+            >
+          </h2>
+          <div class="temp">
+            <h1 class="weather-temp">{{ temperature }}&deg;</h1>
+            <h2 class="text-light">
+              {{ description }} <img :src="iconUrl" :alt="description" />
+            </h2>
+          </div>
+        </div>
+      </div>
+      <div class="card card-2 w-100">
+        <h2 class="text-center mt-2 forecast">Прогноз на 4 дня</h2>
+        <Forecast :cityName="city" />
+        <div id="div_form" class="d-flex m-3 justify-content-center">
+          <form action="">
+            <input
+              type="buton"
+              value="Сменить город"
+              class="btn change_btn btn-primary"
+              @click="changeLocatiob"
+            />
+          </form>
+        </div>
+      </div>
+    </div>
+    <div v-else-if="loading" class="text-center" style="color: white">
+      <h4>Загрузка...</h4>
+    </div>
+    <div v-else class="text-center" style="color: white">
+      <h4>По вашему запросу ничего не найдено</h4>
+    </div>
+  </div>
+</template>
+
+<script>
+import Forecast from "./Forecast.vue";
+import axios from "axios";
+import moment from 'moment';
+import 'moment/dist/locale/ru';
+
+export default {
+  components: {
+    Forecast,
+  },
+  props: {
+    city: String,
+  },
+  data() {
+    return {
+      success: false,
+      loading: true,
+      name: "",
+      country: "",
+      temperature: null,
+      description: null,
+      iconUrl: null,
+      localDate: null,
+      localTime: null
+    };
+  },
+  async created() {
+    this.loading = true;
+    const api_key = "633983ebe5532463350062a62b526b9a";
+    const base_url = "https://api.openweathermap.org/data/2.5/";
+    const weatherData = await axios
+      .get(`${base_url}weather?q=${this.city}&units=metric&APPID=${api_key}`)
+      .then((r) => {
+        this.success = true;
+        this.loading = false;
+        return r.data;
+      })
+      .catch((error) => {
+        console.error("Error featchig data", error);
+        this.success = false;
+        this.loading = false;
+      });
+
+    this.name = weatherData.name;
+    this.country = weatherData.sys.country;
+    this.temperature = Math.round(weatherData.main.temp);
+    this.description = weatherData.weather[0].main;
+
+    this.iconUrl = `https://api.openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+
+    const now = moment(new Date())
+    moment.locale('ru')
+    this.localDate = now.format('dddd DD MMMM YYYY')
+    this.localTime = now.format('HH:mm:ss')
+  },
+  methods: {
+    changeLocatiob() {
+      window.location.reload();
+    }
+  }
+};
+</script>
+
+<style scoped>
+@import url("../assets/css/weather.css");
+</style>
